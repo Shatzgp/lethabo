e7304e006c44af41f5b4a7119156b76b03aa1c47
Shatzgp <122834953+Shatzgp@users.noreply.github.com>
2023-10-11 18:52:36 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,113 @@
+'use strict';
+
+
+
+/**
+ * element toggle function
+ */
+
+const toggleElem = function (elem) { elem.classList.toggle("active"); }
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+for (let i = 0; i < navTogglers.length; i++) {
+  navTogglers[i].addEventListener("click", function () {
+    toggleElem(navbar);
+    toggleElem(overlay);
+  });
+}
+
+
+
+/**
+ * header sticky & back to top button
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+    header.classList.add("header-anim");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+    header.classList.remove("header-anim");
+  }
+});
+
+
+
+/**
+ * search box toggle
+ */
+
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+const searchBox = document.querySelector("[data-search-box]");
+
+for (let i = 0; i < searchTogglers.length; i++) {
+  searchTogglers[i].addEventListener("click", function () {
+    toggleElem(searchBox);
+  });
+}
+
+
+
+/**
+ * whishlist button toggle
+ */
+
+const whishlistBtns = document.querySelectorAll("[data-whish-btn]");
+
+for (let i = 0; i < whishlistBtns.length; i++) {
+  whishlistBtns[i].addEventListener("click", function () {
+    toggleElem(this);
+  });
+}
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
